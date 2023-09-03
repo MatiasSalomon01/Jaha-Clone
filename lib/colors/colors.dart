68a6ee2fa635510b6326604d3714bc82a9661198
=Matias Salomon <matias.salomon@clt.com.py>
2023-09-03 18:54:30 -0400

    orden colors

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -2,10 +2,10 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+const grey = Colors.grey;
+const white = Colors.white;
 const transparent = Colors.transparent;
 const yellow = Color(0xfffdc52c);
-const white = Colors.white;
-const grey = Colors.grey;
 const green = Color(0xff6abf4a);
 const purple = Color(0xffa55a95);
 const lightBlue = Color(0xff00c2de);
